27ddac521b003cfda271a29a821d81c76963f309
Sanjeetkumar Mehta <sanjeetkmehta@gmail.com>
2020-07-05 20:18:25 +0530

    Added api calls using axios

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React, {Component} from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends Component {
+    state = {
+        users: [],
+        loading: false
+    };
+    async componentDidMount() {
+        this.setState({loading: true});
+        const result = await axios.get("https://api.github.com/users");
+        this.setState({users: result.data, loading: false});
+    }
     render() {
         return (
             <div className="App">
-                <Navbar title="Github Finder" icon="fab fa-github" />
+                <Navbar />
                 <div className="container">
-                    <Users />
+                    <Users
+                        loading={this.state.loading}
+                        users={this.state.users}
+                    />
                 </div>
             </div>
         );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,21 @@
 import React, {Component} from "react";
 import UserItems from "./UserItems";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-    state = {
-        users: [
-            {
-                login: "mojombo",
-                id: 1,
-                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo"
-            },
-            {
-                login: "defunkt",
-                id: 2,
-                avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-                html_url: "https://github.com/defunkt"
-            },
-            {
-                login: "pjhyett",
-                id: 3,
-                avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-                html_url: "https://github.com/pjhyett"
-            }
-        ]
-    };
-    render() {
+const Users = ({users, loading}) => {
+    if (loading) {
+        return <Spinner />;
+    } else {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {users.map(user => (
                     <UserItems key={user.id} user={user} />
                 ))}
             </div>
         );
     }
-}
+};
 
 const userStyle = {
     display: "grid",
@@ -41,4 +23,8 @@ const userStyle = {
     gridGap: "1rem"
 };
 
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+};
 export default Users;
