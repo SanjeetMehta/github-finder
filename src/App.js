3e12c94a6a8bb78eeb4e5e7f9a6b2b87282325c0
Sanjeetkumar Mehta <sanjeetkmehta@gmail.com>
2020-07-05 19:12:29 +0530

    Changes some of the class components to functional components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {Component} from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import UserItems from "./components/users/UserItems";
+import Users from "./components/users/Users";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Navbar title="Github Finder" icon="fab fa-github" />
-                <UserItems />
+                <div className="container">
+                    <Users />
+                </div>
             </div>
         );
     }
--- src/components/users/UserItems.js
+++ src/components/users/UserItems.js
@@ -1,34 +1,27 @@
-import React, {Component} from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItems extends Component {
-    constructor() {
-        super();
-        this.state = {
-            id: "1",
-            login: "mojombo",
-            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url: "https://github.com/mojombo"
-        };
-    }
-    render() {
-        const {avatar_url, html_url, id, login} = this.state;
-        return (
-            <div className="card text-center">
-                <img
-                    src={avatar_url}
-                    alt=""
-                    className="round-img"
-                    style={{width: "60px"}}
-                />
-                <h3>{login}</h3>
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">
-                        More
-                    </a>
-                </div>
+const UserItems = ({user: {avatar_url, html_url, id, login}}) => {
+    return (
+        <div className="card text-center">
+            <img
+                src={avatar_url}
+                alt=""
+                className="round-img"
+                style={{width: "60px"}}
+            />
+            <h3>{login}</h3>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">
+                    More
+                </a>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+UserItems.propTypes = {
+    user: PropTypes.object.isRequired
+};
 
 export default UserItems;
